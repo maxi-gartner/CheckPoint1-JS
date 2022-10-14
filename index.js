34ec2d257f19c4406a80dc7956bdf0f3fa4fc524
maxi-gartner <maxi-gartner@hotmail.com>
2022-10-14 17:57:44 -0300

    :tada: Complete checkpoint 1

---
+++ index.js
@@ -0,0 +1,41 @@
+var name_check1= "Maxi" //string
+console.log("name_check1",name_check1)
+
+var last_name_check1= "Gartner" //string
+console.log("last_name_check1",last_name_check1)
+
+var age_check1= 29 //number
+console.log("age_check1",age_check1)
+
+var addres_check1= "Roque Saenz Peña 1155" //string
+console.log("addres_check1",addres_check1)
+
+var city_check1= "Crespo" //string
+console.log("city_check1",city_check1)
+
+var state_check1= "Entre Rios" //string
+console.log("state_check1",state_check1)
+
+var countri_check1= "Argentina" //string
+console.log("countri_check1",countri_check1)
+
+var civil_state_check1= false //boolean
+console.log("civil_state_check1",civil_state_check1)
+
+var telefon_number_check1= 3435329910 //number
+console.log("telefon_number_check1",telefon_number_check1)
+
+const agree_to_let_use_the_information_check1= true //boolean
+console.log("agree_to_let_use_the_information_check1",agree_to_let_use_the_information_check1)
+
+var music_check1= {
+    band: "DEL-822",
+    instrument: ["bass(mainly)","guitar","drums",],
+    vocal: true,
+    members: ["Alcides Holzahier","Christian Galeano","Gartner Maxmimiliano"],
+    songs:[true,7],
+    lives: true
+}
+console.log("music_check1",music_check1)
+
+console.log(`My name is ${name_check1} ${last_name_check1} am ${age_check1} years old, live in ${addres_check1}, ${city_check1}, ${state_check1}, ${countri_check1}, my marital status is ${civil_state_check1}, and my telefon number is ${telefon_number_check1}`)
